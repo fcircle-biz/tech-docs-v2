1d840a5fc1cd387ecc0a435f21eeaa3585897666
satoshi.ichimaru <satoshi.ichimaru@f-circle.biz>
2026-06-17 02:00:04 +0900

    init

--- specs/nextjs-order-management/backend/app/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .customers import router as customers_router
-from .products import router as products_router
-from .orders import router as orders_router
-from .dashboard import router as dashboard_router
